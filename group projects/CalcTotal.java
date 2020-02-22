78a35d095b2e8bac26f7409c505e280f432439d5
andrew <60827847+agblatt@users.noreply.github.com>
2020-02-21 20:54:05 -0500

    Create CalcTotal.java

---
+++ group projects/CalcTotal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; //import scanner
+
+public class CalcTotal {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); //initialize scanner
+
+		System.out.print("Enter the subtotal and a gratuity rate: "); //prompt user to input subtotal and gratuity rate
+		double subTotal = scanner.nextDouble(); //get next double input from user -> subTotal
+		double gratuityRate = scanner.nextDouble(); //get next double input from user -> gratuityRate
+
+		gratuityRate /= 10; //divide gratuityRate by 10 -> gratuityRate = gratuityRate / 10
+		subTotal += gratuityRate; //add gratuityRate to subTotal -> subTotal = subTotal + gratuityRate
+
+		System.out.println("The gratuity is $" + gratuityRate + " and total is $" + subTotal); //print whole statement w/ results
+	}
+}
+
+/*
+Enter the subtotal and a gratuity rate: 10 15
+The gratuity is $1.5 and total is $11.5
+*/
